f8ed0d7a82c0b67f192d0de98ea898c9f16f9da2
shaneabrasaldo <lr.smabrasaldo@mmdc.mcl.edu.ph>
2024-10-01 04:45:25 +0800

    js added for navbar scroll effect !

---
+++ navbar.js
@@ -0,0 +1,10 @@
+
+/*Change background color on scroll */
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header'); 
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
